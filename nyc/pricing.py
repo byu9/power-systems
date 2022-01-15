c2106bb13102f328a408b44f228732f3bff3b369
John Yu <byu9@ncsu.edu>
2022-01-15 16:47:14 -0500

    add read_csv_slices to nyc/pricing.py

--- nyc/pricing.py
+++ nyc/pricing.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # DO NOT REMOVE THIS LINE -- created by John Yu
+import pandas
+import logging
+
 from tools import (
     Remote_File,
     Compressed_File,
@@ -19,3 +22,25 @@ class Realtime_LBMP_Remote_Archive(Remote_File, Compressed_File):
         url = 'http://mis.nyiso.com/public/csv/realtime/{}'.format(
             download_name)
         Remote_File.__init__(self, url, download_name)
+
+
+
+class DataFrame(pandas.DataFrame):
+    @property
+    def _constructor(self):
+        return DataFrame
+
+
+def read_csv_slices(filenames):
+
+    def read_csv(f):
+        logging.info('reading csv file "{}"'.format(f))
+        return pandas.read_csv(f,
+                               header=0,
+                               index_col=0,
+                               parse_dates=[0])
+
+    dataframes = [read_csv(f) for f in filenames]
+    dataframe = pandas.concat(dataframes, axis='index')
+
+    return DataFrame(dataframe)
