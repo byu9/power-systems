3c5b9490602e6b63a7a0bed7590e347b5c90213e
John Yu <byu9@ncsu.edu>
2022-02-14 14:44:27 -0500

    add julian date

--- modules/solar/test_julian.py
+++ modules/solar/test_julian.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import unittest
-from .julian_date import to_julian_date
+from .julian import Julian
 
 class Test_Julian_Date(unittest.TestCase):
-    def test_nrel(self):
+    def test_nrel1(self):
         CASES = (
             # (datetime, julian_date)
             ("January 1, 2000 12:00:00",    2451545),
@@ -16,5 +16,10 @@ class Test_Julian_Date(unittest.TestCase):
         )
 
         for datetime, key in CASES:
-            result = to_julian_date(datetime)
+            result = Julian(datetime, delta_T=0, timezone='GMT').day
             self.assertAlmostEqual(key, result)
+
+    def test_nrel2(self):
+        delta_T = 67
+        julian = Julian('2003-10-17T12:30:30.000-07:00', delta_T)
+        self.assertAlmostEqual(julian.day, 2452930.312847, places=6)
